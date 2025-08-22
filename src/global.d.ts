5ca06b2f679b9bdcc6e6c64c533a4cdd552fe3fd
miss you <1020814597@qq.com>
2025-08-22 16:57:11 +0800

    Update global types to be more comprehensive and EIP-1193 compliant

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,13 +4,18 @@ export {}
 declare global {
   interface Window {
     ethereum?: {
-      isMetaMask?: true
+      isMetaMask?: boolean
       request: (args: { method: string; params?: any[] }) => Promise<any>
       on: (event: string, handler: (...args: any[]) => void) => void
       removeListener: (event: string, handler: (...args: any[]) => void) => void
       selectedAddress?: string
       chainId?: string
       networkVersion?: string
+      // 支持更多的标准 EIP-1193 属性
+      isConnected?: () => boolean
+      enable?: () => Promise<string[]>
+      send?: (method: string, params?: any[]) => Promise<any>
+      sendAsync?: (request: any, callback: (error: any, response: any) => void) => void
     }
   }
 }
